cbc14264ab9add905eb50274ac7982477464d57f
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-06-30 16:43:00 +0300

    chore: Add initial project files and configurations

---
+++ src/Exercises/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("data.txt");
+        Scanner scanner = new Scanner(f);
+    }
+}
